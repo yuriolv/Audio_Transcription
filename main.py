7df8df06a2043b3f75a9d22919c22f9091bc25a8
yuriolv <yuri_2505@icloud.com>
2024-12-03 23:37:55 -0300

    separating the transcribed text into sentences

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from langchain_ollama import OllamaLLM
+import nltk
+
+nltk.download('punkt') 
+
+caminho_arquivo = 'phrases.txt'
+with open(caminho_arquivo, 'r') as arquivo:
+    summary = arquivo.read()
+
+phrases = nltk.sent_tokenize(summary)
 
 
 prompt = """Im sending you one phrase, classify it with only one word, being the word one of the two options: Correct, Wrong(The Corresponding Grammatical or Syntatical Error Type). Here is the phrase: """
 
-caminho_arquivo = 'phrases.txt'
-
-linhas = []
-
-with open(caminho_arquivo, 'r') as arquivo:
-    linhas = arquivo.readlines()
-
-linhas = [linha.strip() for linha in linhas]
 
 
 model = OllamaLLM(model="gemma2:27b") #ollama run llama3.2-vision 11B
 
 i = 0
-while linhas:
-    phrase = prompt + linhas.pop(0)
+while phrases:
+    phrase = prompt + phrases.pop(0)
 
     if phrase.lower() == "exit":
         break
