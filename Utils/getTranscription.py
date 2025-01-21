90769b05e4dbb90a6d417e1bbf2103ea7aa27c39
LAURA-uece <laura@uece.br>
2025-01-21 14:59:58 -0300

    struturing interfaces

--- Utils/getTranscription.py
+++ Utils/getTranscription.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 def get_Transcription(file_name):
-  main_directory = Path("Audios")
+  main_directory = Path("Assets/Transcriptions")
 
   for item in main_directory.iterdir():
       absolute = item.name.split()
--- main.py
+++ main.py
@@ -5,140 +5,167 @@ from Utils.errorDetection import errorDetection
 from Utils.sendMessage import send_message
 from PIL import Image
 
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Error Correction")
+        self.geometry("900x600")
 
+        self.grid_rowconfigure(0, weight=1)  # Permitir que a linha 0 se expanda
+        self.grid_columnconfigure(0, weight=1)
 
-def go_to_second_screen():
+        self.frames = {}
+        self.shared_data = None
 
-    if selected[0]:
-        file_name = selected[0]
-    else:
-        raise print('No files selected')
+        for F in (FirstScreen, SecondScreen):
+            frame = F(parent=self, controller=self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
 
-    transcripted = get_Transcription(file_name)
-    students = errorDetection(transcripted)
+        self.show_frame(FirstScreen)
 
-    
+    def show_frame(self, tela):
+        frame = self.frames[tela]
+        if hasattr(frame, "initialize"):
+            frame.initialize()
+        frame.tkraise()
 
-    for widget in checkbutton_frame.winfo_children():
-        widget.destroy()
+class FirstScreen(ctk.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        self.selected = [None]
 
-    for student in students:
-        for phrase in student.phrases:
-            var = ctk.BooleanVar(value=False)
-            chk = ctk.CTkCheckBox(
-                checkbutton_frame, 
-                text=phrase.content, 
-                variable=var,
-                text_color="white",
-                fg_color="#1D4E89",
-                command=lambda p=phrase: checkbox_changed(p)
-            )
-            chk.pack(fill="x", padx=20, pady=5, anchor="center")
-             
+        # Elementos da Tela Inicial
+        image = Image.open("Assets/Images/logo1.png")  
+        logo = ctk.CTkImage(image, size=(140,100))
+
+        logo_label = ctk.CTkLabel(self, image=logo, text="")
+        logo_label.place(relx=0.5, rely=0.1, anchor="center")
+
+        values = self.get_files()
+
+        combobox = ctk.CTkComboBox(
+            self,
+            values=values,  # Define as opções disponíveis
+            command=self.select_option  # Chama a função quando uma opção é selecionada
+        )
+        combobox.pack(pady=100)
+        combobox.set('Select the lesson')
+
+        self.error_label = ctk.CTkLabel(self, text="", font=ctk.CTkFont(size=14), text_color="red")
+        self.error_label.pack(pady=10)
+
+        button = ctk.CTkButton(self, text="Start the correction", command=lambda: self.go_to_second_screen())
+        button.pack()
+
+    def select_option(self, option):
+        self.error_label.configure(text="")  
+        self.selected[0] = option
+
+    def go_to_second_screen(self):
+
+        if self.selected[0] != None:
+            file_name = self.selected[0]
+            self.controller.shared_data = file_name
+            self.controller.show_frame(SecondScreen)
+        else:
+            self.error_label.configure(text="No file selected! Please select one and try again.")
+            return
+        
+    def get_files(self):
+        main_directory = Path("Assets/Transcriptions")
+        subdirectories = []
+
+        for item in main_directory.iterdir():
+            if item.is_dir():
+                name = item.name.split()
+                subdirectories.append(f'{name[0]} {name[1]}')
+        
+        if not subdirectories: return None
+
+        return subdirectories
+
         
 
-    confirm_button.configure(
-        command=lambda: put_message(students)
-    )
 
-    show_frame(frame2)
+class SecondScreen(ctk.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
 
-def back_to_first_screen():
-    show_frame(frame1)
+        image = Image.open("Assets/Images/logo1.png")  
+        logo = ctk.CTkImage(image, size=(140,100))
 
-def checkbox_changed(phrase):
-    phrase.check = not phrase.check
+        logo_label2 = ctk.CTkLabel(self, image=logo, text="")
+        logo_label2.place(relx=0.8, rely=0.8)
+
+
+        title = ctk.CTkLabel(self, text='Errors detected', font=ctk.CTkFont(size=16, weight="bold"))
+        title.pack(fill='both')
+
+        self.checkbutton_frame = ctk.CTkFrame(self)
+        self.checkbutton_frame.pack(fill="both", pady=40)
+
+        button_frame = ctk.CTkFrame(self)
+        button_frame.pack(anchor="center")
+
+        back_button = ctk.CTkButton(button_frame, text="Back", command=self.back_to_first_screen)
+        back_button.pack(side="left", padx=5)
+
+        self.confirm_button = ctk.CTkButton(button_frame, text="Confirm")
+        self.confirm_button.pack(side='left', padx=5)
+
+
+    def initialize(self):
+        self.clear_checkbutton_frame()
+
+        # Carregar transcrição e dados de erros
+        transcripted = get_Transcription(self.controller.shared_data)
+        students = errorDetection(transcripted)
+
+        for student in students:
+            for phrase in student.phrases:
+                var = ctk.BooleanVar(value=False)
+                chk = ctk.CTkCheckBox(
+                    self.checkbutton_frame, 
+                    text=phrase.content, 
+                    variable=var,
+                    text_color="white",
+                    fg_color="#1D4E89",
+                    command=lambda p=phrase: self.checkbox_changed(p)
+                )
+                chk.pack(fill="x", padx=20, pady=5, anchor="center")
+
+        self.confirm_button.configure(
+            command=lambda: self.put_message(students)
+        )
+
+    def clear_checkbutton_frame(self):
+        """Remove widgets existentes no frame de checkboxes."""
+        for widget in self.checkbutton_frame.winfo_children():
+            widget.destroy()
+
+    def back_to_first_screen(self):
+        self.controller.show_frame(FirstScreen)
+
+    def checkbox_changed(self, phrase):
+        phrase.check = not phrase.check
         
 
-def put_message(students):
-    texts = []
-    for student in students:
-        text = 'Errors detected during the lesson:\n'
-        for index, phrase in enumerate(student.phrases):
-            if phrase.check == True:
-                phrase.content = phrase.content.replace(student.name, '')
-                phrase.content = phrase.content.replace(' - ', '')
-                text += f'{index + 1}) {phrase.content}\n'
-                texts.append(text)
-                send_message(text, student.email) 
-
-def get_files():
-    main_directory = Path("Audios")
-    subdirectories = []
-
-    for item in main_directory.iterdir():
-        if item.is_dir():
-            name = item.name.split()
-            subdirectories.append(f'{name[0]} {name[1]}')
-    
-    if not subdirectories: return None
-
-    return subdirectories
-
-def select_option(option):
-    selected[0] = option
+    def put_message(self, students):
+        texts = []
+        for student in students:
+            text = 'Errors detected during the lesson:\n'
+            for index, phrase in enumerate(student.phrases):
+                if phrase.check == True:
+                    phrase.content = phrase.content.replace(student.name, '')
+                    phrase.content = phrase.content.replace(' - ', '')
+                    text += f'{index + 1}) {phrase.content}\n'
+                    texts.append(text)
+                    send_message(text, student.email) 
 
 
-def show_frame(frame):
-    for widget in root.winfo_children():
-        widget.pack_forget()
-    frame.pack(fill="both", expand=True)
-
-# Configuração principal
-ctk.set_appearance_mode("System")  # "System", "Light" ou "Dark"
-ctk.set_default_color_theme("blue")
-
-root = ctk.CTk()
-root.title("Error Correction")
-root.geometry("900x600")
-
-# Frame 1 (primeira tela)
-frame1 = ctk.CTkFrame(root, corner_radius=0)
-frame1.pack(fill="both", expand=True, anchor="center")
-
-image = Image.open("Assets/logo1.png")  
-logo = ctk.CTkImage(image, size=(140,100))
-
-logo_label = ctk.CTkLabel(frame1, image=logo, text="")
-logo_label.place(relx=0.5, rely=0.1, anchor="center")
-
-selected = [None]
-values = get_files()
-
-combobox = ctk.CTkComboBox(
-    frame1,
-    values=values,  # Define as opções disponíveis
-    command=select_option  # Chama a função quando uma opção é selecionada
-)
-combobox.pack(pady=200)
-combobox.set('Select the lesson')
-
-button = ctk.CTkButton(frame1, text="Start the correction", command=lambda: go_to_second_screen())
-button.pack()
-
-# Frame 2 (segunda tela)
-frame2 = ctk.CTkFrame(root, corner_radius=0)
-
-logo_label2 = ctk.CTkLabel(frame2, image=logo, text="")
-logo_label2.place(relx=0.8, rely=0.8)
-
-
-title = ctk.CTkLabel(frame2, text='Errors detected', font=ctk.CTkFont(size=16, weight="bold"))
-title.pack(fill='both')
-
-checkbutton_frame = ctk.CTkFrame(frame2)
-checkbutton_frame.pack(fill="both", pady=40)
-
-button_frame = ctk.CTkFrame(frame2)
-button_frame.pack(anchor="center")
-
-back_button = ctk.CTkButton(button_frame, text="Back", command=back_to_first_screen)
-back_button.pack(side="left", padx=5)
-
-confirm_button = ctk.CTkButton(button_frame, text="Confirm")
-confirm_button.pack(side='left', padx=5)
-
-root.mainloop()
-
-
-""" Verificação de erros, frontend """
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
