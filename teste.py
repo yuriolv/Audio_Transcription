404d09c50c5b657f0a04378fa0667d4473606a24
LAURA-uece <laura@uece.br>
2025-02-13 15:31:16 -0300

    test

--- teste.py
+++ teste.py
@@ -1,13 +1,22 @@
-import os
-import subprocess
+# This example is the new way to use the OpenAI lib for python
+from openai import OpenAI
 
-# Base do caminho no AppData
-user = os.getlogin()
-base_path = os.path.join(os.environ["LOCALAPPDATA"],"Programs" ,"Ollama", "ollama app.exe")
-print(user)
+client = OpenAI(
+api_key = "LA-246e09a0a1fe47dc9d04f21ec6708b85e8997f2645714d6e914f6ad7ca1cadba",
+base_url = "https://api.llama-api.com"
+)
 
-# Executa o Ollama
-if os.path.exists(base_path):
-    subprocess.run(["runas", f"/user:{user}", base_path])
-else:
-    print("O caminho do Ollama não foi encontrado.")
+response = client.chat.completions.create(
+model="llama3.1-70b",
+messages=[
+    {"role": "system", "content": "Assistant is a large language model trained by OpenAI."},
+    {"role": "user", "content": "Who were the founders of Microsoft?"}
+],
+
+
+)
+
+#print(response)
+print(response.model_dump_json(indent=2))
+print(response.choices[0].message.content)
+
