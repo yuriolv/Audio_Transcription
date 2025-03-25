0b4dbc7103e6e98ff52883e2748ed4eedd533065
yuriolv <yuri_2505@icloud.com>
2025-03-25 13:48:11 -0300

    refatored corrected

--- Database/Corrections.py
+++ Database/Corrections.py
@@ -6,31 +6,31 @@ db_path = "language_school.db"
 def create_correction(correction, id_student, id_transcription):
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO Correction (Correction, Student_Id, Transcription_Id) VALUES (?, ?, ?)", (correction, id_student, id_transcription))
+        cursor.execute("INSERT INTO Correction (Corrections, Student_Id, Transcription_Id) VALUES (?, ?, ?)", (correction, id_student, id_transcription))
         conn.commit()
 
 def read_corrections():
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM Correction")
+        cursor.execute("SELECT * FROM Corrections")
         return cursor.fetchall()
     
 def get_corrections(student_id):
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
-        cursor.execute("SELECT Phrase,Error FROM Correction WHERE Student_Id = ?", (student_id,))
+        cursor.execute("SELECT Phrase,Error FROM Corrections WHERE Student_Id = ?", (student_id,))
         return cursor.fetchall()[-3:]
 
 def update_correction(correction_id, correction, id_student, id_transcription):
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
-        cursor.execute("UPDATE Correction SET Correction = ?, Student_Id = ?, Transcription_Id = ? WHERE Id = ?", (correction, id_student, id_transcription, correction_id))
+        cursor.execute("UPDATE Corrections SET Correction = ?, Student_Id = ?, Transcription_Id = ? WHERE Id = ?", (correction, id_student, id_transcription, correction_id))
         conn.commit()
 
 def delete_correction(correction_id):
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM Correction WHERE Id = ?", (correction_id,))
+        cursor.execute("DELETE FROM Corrections WHERE Id = ?", (correction_id,))
         conn.commit()
 
 get_corrections(1)
