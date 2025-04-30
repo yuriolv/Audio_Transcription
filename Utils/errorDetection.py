322c6ddb45f8da6c1762631d16c9134c4f7a7bc8
PudimDeOvo <183882067+PudimDeOvo@users.noreply.github.com>
2025-04-29 22:48:08 -0300

    temporary model change

--- Utils/errorDetection.py
+++ Utils/errorDetection.py
@@ -10,7 +10,8 @@ def errorDetection(transcripted):
 
     prompt1 = """Im sending you one phrase, classify it with only one word, being the word one of the two options: Correct, Wrong(The Corresponding Grammatical or Syntatical Error Type). Here is the phrase: """
 
-    model = OllamaLLM(model="gemma3:27b") #ollama run llama3.2-vision 11B
+    #model = OllamaLLM(model="gemma3:27b") #ollama run llama3.2-vision 11B
+    model = OllamaLLM(model="llama3.2")
 
     for user in transcripted.students:
         for phrase in user.phrases[:]:
